824b90a0febf1777ddc3a84c5ecf43caa6b36d6c
Ali Cihan Isikal <cihanisikal@outlook.com>
2025-02-22 06:21:00 +0300

    Add Terraform configuration for Google Kubernetes Engine cluster and node pools

---
+++ main.tf
@@ -0,0 +1,52 @@
+provider "google" {
+  project = "PROJECT_ID"
+  region  = "europe-west1"
+}
+
+resource "google_container_cluster" "gke_cluster" {
+  name     = "wingie-case-gke-cluster"
+  location = "europe-west1"
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  logging_service    = "none"
+  monitoring_service = "none"
+
+  network    = "default"
+  subnetwork = "default"
+}
+
+resource "google_container_node_pool" "main_pool" {
+  name       = "main-pool"
+  cluster    = google_container_cluster.gke_cluster.id
+  location   = "europe-west1"
+  node_count = 1
+
+  node_config {
+    machine_type = "n2d-standard-2"
+    preemptible  = false
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
+
+resource "google_container_node_pool" "application_pool" {
+  name       = "application-pool"
+  cluster    = google_container_cluster.gke_cluster.id
+  location   = "europe-west1"
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 3
+  }
+
+  node_config {
+    machine_type = "n2d-standard-2"
+    preemptible  = false
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
